7bad85ce7d38f17e4495d05d2339512bf6b22cca
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-12-02 12:55:33 -0300

    Update

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,55 @@
 const addBtn = document.getElementById('addBtn');
 const cancelBtn = document.getElementById('cancelBtn');
 const modal = document.getElementById('modal');
+const waitingContainer = document.getElementById('waiting');
+const onGoingContainer = document.getElementById('onGoing');
+const finishedContainer = document.getElementById('finished');
+
+
 
 const statusRevItens = document.querySelectorAll('.statusReserva');
+let waiting = 0
+let onGoing = 0
+let finished = 0
+
 statusRevItens.forEach(statusRevItem => {
   if (statusRevItem.value === '0') {
     statusRevItem.value = 'Em espera';
     statusRevItem.style.backgroundColor = '#B87457';
+    waiting++
   } else if (statusRevItem.value === '1') {
     statusRevItem.value = 'Em andamento';
     statusRevItem.style.backgroundColor = '#4585A8';
+    onGoing++
   } else if (statusRevItem.value === '2') {
     statusRevItem.value = 'Finalizada';
     statusRevItem.style.backgroundColor = '#3C9786';
+    finished++
   } else{
     statusRevItem.value = 'Algo de errado';
     statusRevItem.style.backgroundColor = '#6C6C6C';
   }
 });
+
+if (waiting <1){
+    waitingContainer.innerHTML = `Não existe Nenhuma reserva em espera`
+}else{
+    waitingContainer.innerHTML = `Existem ${waiting} reservas que estão na fila de espera`
+}
+
+if (onGoing<1){
+    onGoingContainer.innerHTML = `Não existe Nenhuma reserva em endamento`
+}else{
+    onGoingContainer.innerHTML = `Existem ${onGoing} reservas que estão em processo de andamento`
+}
+
+if (finished<1){
+    finishedContainer.innerHTML = `Não existe Nenhuma reserva concluída`
+}else{
+    finishedContainer.innerHTML = `Existem ${finished} reservas que foram finalizadas com sucesso`
+}
+
+
 
 
 addBtn.addEventListener('click',()=>{
