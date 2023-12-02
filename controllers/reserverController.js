dfd667aa7a1c995e8e967cc2d7b0f22613fad6ea
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-12-02 11:38:08 -0300

    Update

--- controllers/reserverController.js
+++ controllers/reserverController.js
@@ -12,15 +12,17 @@ const listTable = async (req, res) => {
     let reservas = await acomodacao.findAll({ raw: true, limit: 6 });
 
     reservas = reservas.map((reserva) => {
+
+        const meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
         const data_entrada = new Date(reserva.data_entrada);
         const data_saida = new Date(reserva.data_saida);
-        reserva.data_entrada = `${data_entrada.getDate()}/${data_entrada.getMonth()}/${data_entrada.getFullYear()}`
-        reserva.data_saida = `${data_saida.getDate()}/${data_saida.getMonth()}/${data_saida.getFullYear()}`
+        reserva.data_entrada = `${data_entrada.getDate()} de ${meses[data_entrada.getMonth()]} de ${data_entrada.getFullYear()}`;
+        reserva.data_saida = `${data_saida.getDate()} de ${meses[data_saida.getMonth()]} de ${data_saida.getFullYear()}`;
 
         return reserva
     })
 
-    res.render("home", {reservas})
+    res.render("home", { reservas })
 }
 const historicoGeral = async (req, res) => {
     let reservas = await acomodacao.findAll({ raw: true });
@@ -33,7 +35,7 @@ const historicoGeral = async (req, res) => {
 
         return reserva
     })
-    res.render('history', {reservas})
+    res.render('history', { reservas })
 }
 
-module.exports = { ControleReservas, listTable,historicoGeral }
+module.exports = { ControleReservas, listTable, historicoGeral }
--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,18 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("db_easyhost", "aluno_medio", "@lunoSenai23.", {
+const sequelize = new Sequelize("db_easyhost", "tony", "R@dri8669tony", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
 });
 
 module.exports = sequelize;
+// const { Sequelize } = require("sequelize");
+
+// const sequelize = new Sequelize("db_easyhost", "aluno_medio", "@lunoSenai23.", {
+//   host: "localhost",
+//   port: 3306,
+//   dialect: "mysql",
+// });
+
+// module.exports = sequelize;
