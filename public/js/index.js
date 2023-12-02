be3fd09ef4f2c978b3331f1fa745108436dc6048
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-12-02 12:34:40 -0300

    Update

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,24 @@ const addBtn = document.getElementById('addBtn');
 const cancelBtn = document.getElementById('cancelBtn');
 const modal = document.getElementById('modal');
 
+const statusRevItens = document.querySelectorAll('.statusReserva');
+statusRevItens.forEach(statusRevItem => {
+  if (statusRevItem.value === '0') {
+    statusRevItem.value = 'Em espera';
+    statusRevItem.style.backgroundColor = '#B87457';
+  } else if (statusRevItem.value === '1') {
+    statusRevItem.value = 'Em andamento';
+    statusRevItem.style.backgroundColor = '#4585A8';
+  } else if (statusRevItem.value === '2') {
+    statusRevItem.value = 'Finalizada';
+    statusRevItem.style.backgroundColor = '#3C9786';
+  } else{
+    statusRevItem.value = 'Algo de errado';
+    statusRevItem.style.backgroundColor = '#6C6C6C';
+  }
+});
+
+
 addBtn.addEventListener('click',()=>{
     modal.style.display = 'flex'
 })
