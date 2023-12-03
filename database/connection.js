2140e77f6311b17b180101c252e9e7e697239cb3
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-12-02 23:35:06 -0300

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("db_easyhost", "tony", "R@dri8669tony", {
+const sequelize = new Sequelize("db_easyhost", "tony", "", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
